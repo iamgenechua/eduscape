649cef4b1a884a0b01ac3cd27f679d5110d07ab8
hellodommy <dominique@u.nus.edu>
2021-03-19 14:18:42 +0800

    Stop door from rotating after open

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
 
-    public bool isOpen;
+    public bool shouldOpen;
 
     // Start is called before the first frame update
     void Start()
     {
-        isOpen = false;
+        shouldOpen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isOpen)
+        if (shouldOpen)
         {
             gameObject.transform.Rotate(0.0f, 90.0f, 0.0f, Space.World);
+            shouldOpen = false;
         }
         
     }
@@ -29,7 +30,7 @@ public class OpenDoor : MonoBehaviour
         
         if (collidedObject.GetComponent<Element>().ElementType == Element.Type.WATER || collidedObject.GetComponent<Element>().ElementType == Element.Type.AIR)
         {
-            isOpen = true;
+            shouldOpen = true;
         }
     }
 }
