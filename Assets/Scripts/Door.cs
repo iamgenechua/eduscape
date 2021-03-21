57f7437922bfe8f5d19cde37633d990870622006
Kenneth Fung <fungchenyu@rocketmail.com>
2021-03-21 18:07:53 +0800

    Start using sci fi pack with door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,17 +6,25 @@ public class Door : MonoBehaviour {
 
     private Animator anim;
 
+    [SerializeField] private string animOpenBool;
+
+    [Tooltip("Object, with colliders, to activate when door is closed.")]
+    [SerializeField] private GameObject closedColliders;
+
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
+        closedColliders.SetActive(!anim.GetBool(animOpenBool));
     }
 
     public void OpenDoor() {
-        anim.SetBool("isOpen", true);
+        anim.SetBool(animOpenBool, true);
+        closedColliders.SetActive(false);
     }
 
     public void CloseDoor() {
-        anim.SetBool("isOpen", false);
+        anim.SetBool(animOpenBool, false);
+        closedColliders.SetActive(true);
     }
 
     public void OpenDoor(Element element) {
