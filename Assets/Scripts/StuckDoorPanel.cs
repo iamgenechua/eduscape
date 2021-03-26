188f414bf8df403e97a107db5bf1077641543406
Kenneth Fung <fungchenyu@rocketmail.com>
2021-03-27 00:45:51 +0800

    Make puzzle 1 door panel fall off

---
+++ Assets/Scripts/StuckDoorPanel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuckDoorPanel : MonoBehaviour {
+
+    private Rigidbody rb;
+    private ElementTarget elementTarget;
+
+    [SerializeField] private RationaleCanvas rationaleCanvas;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        elementTarget = GetComponent<ElementTarget>();
+    }
+
+    public void HitByElement(Element element) {
+        if (element.ElementType != Element.Type.FIRE) {
+            rb.isKinematic = false;
+            elementTarget.enabled = false;
+            rationaleCanvas.FadeIn();
+        }
+    }
+}
