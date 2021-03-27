672b75e223b48ec43deb890daec24ce897d9d358
Kenneth Fung <fungchenyu@rocketmail.com>
2021-03-27 15:08:26 +0800

    Fade in piston rationale when puzzle complete

--- Assets/Scripts/Piston/Piston.cs
+++ Assets/Scripts/Piston/Piston.cs
@@ -61,6 +61,6 @@ public class Piston : MonoBehaviour {
         }
 
         transform.Translate(Vector3.down * currSpeed * Time.deltaTime);
-        platform.Move(currSpeed * pistonToPlatformSpeedRatio);
+        platform.ChangeSpeed(currSpeed * pistonToPlatformSpeedRatio);
     }
 }
---
+++ Assets/Scripts/Piston/PistonPlatform.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PistonPlatform : MonoBehaviour {
+
+    [SerializeField] private float maxLocalY;
+    [SerializeField] private float minLocalY;
+
+    [SerializeField] private UnityEvent reachTopEvent;
+    [SerializeField] private UnityEvent reachBottomEvent;
+
+    private float currSpeed = 0f;
+    private bool isMoving = false;
+
+    public bool HasReachedTop { get => transform.localPosition.y > maxLocalY; }
+    public bool HasReachedBottom { get => transform.localPosition.y < minLocalY; }
+
+    void Update() {
+        if (isMoving) {
+            Move(currSpeed);
+        }
+    }
+
+    public float GetMoveableDistance() {
+        return maxLocalY - minLocalY;
+    }
+
+    public void StartMoving() {
+        isMoving = true;
+    }
+
+    public void StopMoving() {
+        isMoving = false;
+    }
+
+    public void ChangeSpeed(float speed) {
+        if (!isMoving) {
+            StartMoving();
+        }
+
+        currSpeed = speed;
+    }
+
+    private void Move(float speed) {
+        if (HasReachedTop) {
+            reachTopEvent.Invoke();
+        } else if (HasReachedBottom) {
+            reachBottomEvent.Invoke();
+        }
+
+        if (HasReachedTop || HasReachedBottom) {
+            StopMoving();
+            return;
+        }
+
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
