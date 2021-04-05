d4b28ed9b82af34234befb0e5af445ed599debc2
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-05 18:52:24 +0800

    Separate game/level logic and create main menu scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     private static GameManager _instance;
     public static GameManager Instance { get => _instance; }
-
-    [SerializeField] private GameObject player;
-    public GameObject Player { get => player; }
 
     [SerializeField] private FadePlayerView fade;
 
@@ -18,34 +13,10 @@ public class GameManager : MonoBehaviour {
         // singleton
         if (_instance != null && _instance != this) {
             Destroy(gameObject);
-        } else {
+        }
+        else {
             _instance = this;
         }
-    }
-
-    // Start is called before the first frame update
-    void Start() {
-        StartCoroutine(StartGame());
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
-    }
-
-    private IEnumerator StartGame() {
-        yield return new WaitForSeconds(3f);
-        fade.FadeIn();
-        TutorialManager.Instance.StartTutorial();
-    }
-
-    public void RestartGame() {
-        if (fade.IsFading) {
-            return;
-        }
-
-        fade.FadeOutCompleteEvent.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
-        fade.FadeOut();
     }
 
     public void QuitGame() {
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    private static LevelManager _instance;
+    public static LevelManager Instance { get => _instance; }
+
+    [SerializeField] private GameObject player;
+    public GameObject Player { get => player; }
+
+    [SerializeField] private FadePlayerView fade;
+
+    void Awake() {
+        // singleton
+        if (_instance != null && _instance != this) {
+            Destroy(gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(StartLevel());
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private IEnumerator StartLevel() {
+        yield return new WaitForSeconds(3f);
+        fade.FadeIn();
+        TutorialManager.Instance.StartTutorial();
+    }
+
+    public void RestartLevel() {
+        if (fade.IsFading) {
+            return;
+        }
+
+        fade.FadeOutCompleteEvent.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+        fade.FadeOut();
+    }
+}
