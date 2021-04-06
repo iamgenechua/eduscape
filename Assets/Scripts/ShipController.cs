898a24dbc7b1b3132a798d38f40610302ce76df8
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-06 17:17:28 +0800

    Close ramp when ship launches

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
 
+    [Header("Cockpit Door")]
+
+    [SerializeField] private Door cockpitDoor;
+
+    [Header("Ramp")]
+
+    [SerializeField] private BoxCollider rampClosingCollider;
+
+    [SerializeField] private Animator rampAnim;
+    [SerializeField] private string rampAnimParam = "isOpen";
+
+    [Header("Summary and Button Stations")]
+
     [SerializeField] private FadeCanvas[] screenFadeCanvases;
     [SerializeField] private Animator[] buttonCoverAnims;
     [SerializeField] private string buttonCoverAnimParam = "isOpen";
@@ -13,6 +26,8 @@ public class ShipController : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         HasLaunched = false;
+        cockpitDoor.OpenDoor();
+        rampClosingCollider.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,6 +38,18 @@ public class ShipController : MonoBehaviour {
     public void Launch() {
         // blast off into space
         HasLaunched = true;
+        CloseRamp();
+        UnlockSummaryAndButtons();
+    }
+
+    public void CloseRamp() {
+        rampAnim.SetBool(rampAnimParam, false);
+
+        // prevent the player from teleporting out of the ship while the ramp is closing
+        rampClosingCollider.gameObject.SetActive(true);
+        StartCoroutine(GameManager.Instance.WaitForConditionBeforeAction(
+            () => rampAnim.GetCurrentAnimatorStateInfo(0).IsName("Ramp Closed"),
+            () => rampClosingCollider.gameObject.SetActive(false)));
     }
 
     public void UnlockSummaryAndButtons() {
