a0060e9587cb21839158a421fd9fa641aceaed2b
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-06 18:44:02 +0800

    Change implementation of pressable button

--- Assets/Scripts/PressableButton.cs
+++ Assets/Scripts/PressableButton.cs
@@ -5,36 +5,60 @@ using UnityEngine.Events;
 
 public class PressableButton : MonoBehaviour {
 
+    private Rigidbody rb;
+
     [SerializeField] private float pressLength;
-    private Vector3 startPos;
+    [SerializeField] private float releaseSpeed;
+    private float startHeight;
 
     public bool IsPressed { get; private set; }
+    private bool isPressing = false;
 
     [SerializeField] private UnityEvent pressedEvent;
     [SerializeField] private UnityEvent releasedEvent;
 
     // Start is called before the first frame update
     void Start() {
-        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+        startHeight = transform.localPosition.y;
     }
 
     // Update is called once per frame
     void Update() {
-        float distanceFromStartPos = Mathf.Abs(transform.position.y - startPos.y);
-        if (distanceFromStartPos >= pressLength) {
-            transform.position = new Vector3(transform.position.x, startPos.y - pressLength, transform.position.z);
-            if (!IsPressed) {
-                IsPressed = true;
-                pressedEvent.Invoke();
+        float distanceFromStartPos = Mathf.Abs(transform.localPosition.y - startHeight);
+
+        if (isPressing) {
+            if (distanceFromStartPos >= pressLength) {
+                transform.localPosition = new Vector3(transform.localPosition.x, startHeight - pressLength, transform.localPosition.z);
+                rb.velocity = Vector3.zero;
+                if (!IsPressed) {
+                    IsPressed = true;
+                    pressedEvent.Invoke();
+                }
             }
-        } else if (IsPressed && distanceFromStartPos < pressLength / 2f) {
-            // we consider the button released if it's more than half way up
-            IsPressed = false;
-            releasedEvent.Invoke();
         }
 
-        if (transform.position.y > startPos.y) {
-            transform.position = startPos;
+        if (!isPressing) {
+            if (distanceFromStartPos > 0.01f) {
+                transform.Translate(Vector3.up * releaseSpeed * Time.deltaTime, Space.Self);
+            } else {
+                transform.localPosition = new Vector3(transform.localPosition.x, startHeight, transform.localPosition.z);
+                rb.velocity = Vector3.zero;
+            }
+
+            if (IsPressed && distanceFromStartPos < pressLength / 2f) {
+                // we consider the button released if it's more than half way up
+                IsPressed = false;
+                releasedEvent.Invoke();
+            }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        isPressing = true;
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        isPressing = false;
     }
 }
