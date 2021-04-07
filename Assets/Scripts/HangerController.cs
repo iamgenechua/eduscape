dca3920108ef781f75776e9e32118b459b289c83
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-07 11:26:12 +0800

    Activate hanger bay sign after platform puzzle complete

--- Assets/Scripts/HangerController.cs
+++ Assets/Scripts/HangerController.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class HangerController : MonoBehaviour {
 
+    [SerializeField] private Light[] lights;
+    [SerializeField] private Canvas signCanvas;
+
     [SerializeField] private Animator[] wallAnims;
     [SerializeField] private string wallAnimParam = "isOpen";
 
     // Start is called before the first frame update
     void Start() {
-        
+        DeactivateSign();
     }
 
     // Update is called once per frame
     void Update() {
         
+    }
+
+    public void ActivateSign() {
+        StartCoroutine(ActivateSignDramatic());
+    }
+
+    private IEnumerator ActivateSignDramatic() {
+        yield return new WaitForSeconds(3f);
+
+        foreach (Light light in lights) {
+            light.gameObject.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        signCanvas.gameObject.SetActive(true);
+    }
+
+    private void DeactivateSign() {
+        foreach (Light light in lights) {
+            light.gameObject.SetActive(false);
+        }
+
+        signCanvas.gameObject.SetActive(false);
     }
 
     public void OpenWalls() {
