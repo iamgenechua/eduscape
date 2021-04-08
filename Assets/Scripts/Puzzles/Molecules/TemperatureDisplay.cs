633050c491a66f635c0f012613c06e0f1c0afdeb
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-08 17:56:07 +0800

    Add temperature display sfx

--- Assets/Scripts/Puzzles/Molecules/TemperatureDisplay.cs
+++ Assets/Scripts/Puzzles/Molecules/TemperatureDisplay.cs
@@ -6,22 +6,45 @@ using TMPro;
 public class TemperatureDisplay : MonoBehaviour {
 
     private TextMeshPro text;
+    private AudioSource audioSource;
 
     [SerializeField] private TMP_FontAsset defaultFontAsset;
     [SerializeField] private TMP_FontAsset dangerFontAsset;
 
+    [SerializeField] private AudioClip tempIncreaseClip;
+    [SerializeField] private AudioClip maxTempClip;
+    [SerializeField] private float maxTempSoundLoopCount;
+
     void Awake() {
-        text = GetComponent<TextMeshPro>();
+        text = GetComponentInChildren<TextMeshPro>();
+        audioSource = GetComponentInChildren<AudioSource>();
+    }
+
+    private void Start() {
+        audioSource.clip = tempIncreaseClip;
     }
 
     public void UpdateDisplay(float newTemp, bool isTempAboveMax) {
         text.text = newTemp.ToString("0.0") + " °C";
+
         if (isTempAboveMax) {
             SwitchFontAsset(dangerFontAsset);
+            StartCoroutine(PlayMaxTempSound());
+        } else {
+            audioSource.Play();
         }
     }
 
     private void SwitchFontAsset(TMP_FontAsset asset) {
         text.font = asset;
+    }
+
+    private IEnumerator PlayMaxTempSound() {
+        audioSource.clip = maxTempClip;
+
+        for (int i = 0; i < maxTempSoundLoopCount; i++) {
+            audioSource.Play();
+            yield return new WaitUntil(() => !audioSource.isPlaying);
+        }
     }
 }
