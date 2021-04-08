bc9e3ea7072866ef6ff109075314e1f196ec1a0e
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-08 23:50:56 +0800

    Change exterior gate switch to round pressable button prefab

--- Assets/Scripts/Doors/ExteriorGateSwitch.cs
+++ Assets/Scripts/Doors/ExteriorGateSwitch.cs
@@ -7,25 +7,28 @@ public class ExteriorGateSwitch : MonoBehaviour {
     [Tooltip("The local Y value to raise the switch to.")]
     [SerializeField] private float raiseToLocalYValue;
 
+    [Space(10)]
+
     [SerializeField] private float preRaisePauseLength = 1.5f;
     [SerializeField] private float raiseSpeed = 1f;
 
+    [Space(10)]
+
     [SerializeField] private GameObject objectToRaise;
+
+    [Space(10)]
 
     [Tooltip("Because we cannot move the actual switch, we move the dummy switch, then swap it out for the actual switch once in position.")]
     [SerializeField] private GameObject dummySwitch;
     [SerializeField] private GameObject actualSwitch;
 
-    private AudioSource audioSource;
+    [Space(10)]
 
+    [SerializeField] private AudioSource raiseAudioSource;
     [SerializeField] private AudioClip raiseClip;
     [SerializeField] private AudioClip stopClip;
 
     private bool isRising = false;
-
-    void Awake() {
-        audioSource = GetComponentInChildren<AudioSource>();
-    }
 
     public void StartRaise() {
         StartCoroutine(Raise());
@@ -36,16 +39,16 @@ public class ExteriorGateSwitch : MonoBehaviour {
 
         yield return new WaitForSeconds(preRaisePauseLength);
 
-        audioSource.clip = raiseClip;
-        audioSource.Play();
+        raiseAudioSource.clip = raiseClip;
+        raiseAudioSource.Play();
 
         while (objectToRaise.transform.localPosition.y < raiseToLocalYValue) {
             objectToRaise.transform.Translate(new Vector3(0, raiseSpeed * Time.deltaTime, 0));
             yield return null;
         }
 
-        audioSource.clip = stopClip;
-        audioSource.Play();
+        raiseAudioSource.clip = stopClip;
+        raiseAudioSource.Play();
 
         actualSwitch.SetActive(true);
         dummySwitch.SetActive(false);
