be49078b6a42a7a4d470002a844c255394f3eb98
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-09 10:08:52 +0800

    Add exterior gate sfx

--- Assets/Scripts/Doors/ExteriorGate.cs
+++ Assets/Scripts/Doors/ExteriorGate.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class ExteriorGate : MonoBehaviour {
 
     private Animator anim;
+    private AudioSource audioSource;
+
+    [Header("Animation")]
 
     [SerializeField] private string openBooleanParameter;
     [SerializeField] private string openingAnimationTag;
     [SerializeField] private string closingAnimationTag;
+
+    [Header("Audio")]
+
+    [SerializeField] private AudioClip startSound;
+    [SerializeField] private AudioClip creakSound;
+    [SerializeField] private AudioClip impactSound;
+    [SerializeField] private AudioClip impactReverbSound;
+
+    [Header("Action Blockers")]
 
     [Tooltip("The action blocker in the negative direction of the x axis.")]
     [SerializeField] private ActionBlocker actionBlockerLeft;
@@ -20,6 +32,7 @@ public class ExteriorGate : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
+        audioSource = GetComponentInChildren<AudioSource>();
         ActivateActionBlocker();
     }
 
@@ -60,5 +73,21 @@ public class ExteriorGate : MonoBehaviour {
         } else {
             Open();
         }
+    }
+
+    public void PlayStartSound() {
+        audioSource.PlayOneShot(startSound);
+    }
+
+    public void PlayCreakSouund() {
+        audioSource.PlayOneShot(creakSound);
+    }
+
+    public void PlayImpactSound() {
+        audioSource.PlayOneShot(impactSound);
+    }
+
+    public void PlayImpactReverbSound() {
+        audioSource.PlayOneShot(impactReverbSound);
     }
 }
