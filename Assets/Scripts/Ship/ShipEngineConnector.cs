a951c586e2167d01a3c5da8f42b1edc028a77cba
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-09 20:18:39 +0800

    Change structure of engine connector classes

--- Assets/Scripts/Ship/ShipEngineConnector.cs
+++ Assets/Scripts/Ship/ShipEngineConnector.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ShipEngineConnector : MonoBehaviour {
 
-    public enum Type { BASE, METAL, WATER }
-
-    private Type type = Type.BASE;
-
     [SerializeField] private ShipEngine engine;
-
-    [SerializeField] private float timeToHeatSegment;
-
-    [SerializeField] private ShipEngineConnectorSegment[] segments;
+    [SerializeField] private ShipEngineConnectorSegment firstSegment;
 
     private bool isHeating = false;
 
@@ -28,47 +21,16 @@ public class ShipEngineConnector : MonoBehaviour {
 
     public void GeneratorHitByElement(Element element) {
         if (!isHeating && element.ElementType == Element.Type.FIRE) {
-            StartCoroutine(HeatEngines());
+            StartHeating();
         }
     }
 
-    private bool CanContinueConducting(int numSegmentsHeated) {
-        switch (type) {
-            case Type.BASE:
-                return numSegmentsHeated < segments.Length / 2;
-            case Type.METAL:
-                return true;
-            case Type.WATER:
-                return numSegmentsHeated < 1;
-            default:
-                throw new System.ArgumentException($"Conductivity of {type} not accounted for.");
-        }
-    }
-
-    private IEnumerator HeatEngines() {
+    private void StartHeating() {
         isHeating = true;
-        bool areEnginesHeated = true;
-
-        for (int i = 0; i < segments.Length; i++) {
-            yield return new WaitForSeconds(timeToHeatSegment);
-
-            if (CanContinueConducting(i)) {
-                segments[i].Heat();
-                continue;
-            }
-
-            areEnginesHeated = false;
-            break;
-        }
-
-        if (areEnginesHeated) {
-            engine.Heat();
-        } else {
-            foreach (ShipEngineConnectorSegment segment in segments) {
-                segment.Cool();
-            }
-        }
-
-        isHeating = false;
+        float inputEnergy = 100f;
+        firstSegment.Heat(inputEnergy, inputEnergy, () => {
+            isHeating = false;
+            firstSegment.Cool();
+        });
     }
 }
--- Assets/Scripts/Ship/ShipEngineConnectorSegment.cs
+++ Assets/Scripts/Ship/ShipEngineConnectorSegment.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ShipEngineConnectorSegment : MonoBehaviour {
 
+    /// <summary>
+    /// The amount of energy conserved after heating a segment of the given state.
+    /// </summary>
+    private static readonly Dictionary<State, float> stateEnergyConservedDict = new Dictionary<State, float>() {
+        [State.BASE] = 0.5f,
+        [State.METAL] = 1f,
+        [State.WATER] = 0f
+    };
+
+    public enum State { BASE, METAL, WATER }
+
+    private State state = State.BASE;
+
+    [SerializeField] private Material heatedMaterial;
+
+    private MeshRenderer mesh;
+    private Material currUnheatedMaterial;
     private bool isHeated = false;
+
+    [SerializeField] private ShipEngineConnectorSegment[] connectedSegments;
+    [SerializeField] private ShipEngine engine;
+
+    void Awake() {
+        mesh = GetComponent<MeshRenderer>();
+    }
 
     // Start is called before the first frame update
     void Start() {
-        
+        currUnheatedMaterial = mesh.material;
     }
 
     // Update is called once per frame
@@ -16,12 +41,33 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
         
     }
 
-    public void Heat() {
+    public void ChangeState(State newState) {
+        state = newState;
+    }
+
+    public void Heat(float currEnergy, float totalEnergy, UnityAction failureCallback) {
         if (isHeated) {
             return;
         }
 
+        currEnergy -= stateEnergyConservedDict[state] * totalEnergy;
+
+        if (currEnergy <= 0f) {
+            isHeated = false;
+            failureCallback();
+            return;
+        }
+
         isHeated = true;
+        mesh.material = heatedMaterial;
+
+        foreach (ShipEngineConnectorSegment segment in connectedSegments) {
+            segment.Heat(currEnergy, totalEnergy, failureCallback);
+        }
+
+        if (engine != null) {
+            engine.Heat();
+        }
     }
 
     public void Cool() {
@@ -30,5 +76,10 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
         }
 
         isHeated = false;
+        mesh.material = currUnheatedMaterial;
+
+        foreach (ShipEngineConnectorSegment segment in connectedSegments) {
+            segment.Cool();
+        }
     }
 }
