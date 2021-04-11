2628fa9a45a901112dfbb9013c07ff3f13a7f2c1
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-11 09:02:03 +0800

    Add element traveling sfx

--- Assets/Scripts/Elements/ElementProjectile.cs
+++ Assets/Scripts/Elements/ElementProjectile.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class ElementProjectile : Element {
 
+    protected AudioSource audioSource;
+
+    protected override void Awake() {
+        base.Awake();
+        audioSource = GetComponentInChildren<AudioSource>();
+    }
+
+    protected override void Start() {
+        base.Start();
+        audioSource.Play();
+    }
+
     protected void OnCollisionEnter(Collision collision) {
         if (!collision.gameObject.CompareTag("Hand") && !collision.gameObject.CompareTag("Player")) {
             ElementTarget elementTarget = collision.gameObject.GetComponent<ElementTarget>();
