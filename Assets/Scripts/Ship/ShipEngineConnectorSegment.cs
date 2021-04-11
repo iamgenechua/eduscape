554074a961af72ff338a64fb7f3d4834f294c58f
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-11 19:34:28 +0800

    Refactor heated segment scaling into HeatedSegment class

--- Assets/Scripts/Ship/ShipEngineConnectorSegment.cs
+++ Assets/Scripts/Ship/ShipEngineConnectorSegment.cs
@@ -14,18 +14,13 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
         [State.WATER] = 2f
     };
 
-    private static readonly float heatedVisualScaleSpeed = 0.4f;
-
     public enum State { BASE, METAL, WATER }
 
     private State state = State.BASE;
 
-    public enum HeatedSegmentScaleAxis { X, Y, Z }
-
-    [SerializeField] private Transform heatedSegmentVisual;
-    [SerializeField] private HeatedSegmentScaleAxis heatedVisualScaleAxis;
-
     private MeshRenderer mesh;
+
+    [SerializeField] private HeatedSegment heatedSegment;
 
     public bool IsHeated { get; private set; }
 
@@ -34,23 +29,6 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
 
     void Awake() {
         mesh = GetComponent<MeshRenderer>();
-    }
-
-    // Start is called before the first frame update
-    void Start() {
-        switch (heatedVisualScaleAxis) {
-            case HeatedSegmentScaleAxis.X:
-                heatedSegmentVisual.localScale = new Vector3(0f, heatedSegmentVisual.localScale.y, heatedSegmentVisual.localScale.z);
-                break;
-            case HeatedSegmentScaleAxis.Y:
-                heatedSegmentVisual.localScale = new Vector3(heatedSegmentVisual.localScale.x, 0f, heatedSegmentVisual.localScale.z);
-                break;
-            case HeatedSegmentScaleAxis.Z:
-                heatedSegmentVisual.localScale = new Vector3(heatedSegmentVisual.localScale.x, heatedSegmentVisual.localScale.y, 0f);
-                break;
-        }
-
-        heatedSegmentVisual.gameObject.SetActive(false);
     }
 
     public void ChangeState(State newState, Material newMaterial) {
@@ -67,32 +45,12 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
     }
 
     private IEnumerator StartHeating(float currEnergy, float totalEnergy, UnityAction failureCallback) {
-        heatedSegmentVisual.gameObject.SetActive(true);
-
+        // only heat the entire segment if we have not run out of energy
         currEnergy -= stateEnergyLostDict[state] * totalEnergy;
         bool isEnergySufficient = currEnergy >= 0f;
+        StartCoroutine(heatedSegment.Heat(isEnergySufficient ? 0.99f : 0.5f));
 
-        // only heat the entire segment if we have not run out of energy
-        float maxScaleValue = isEnergySufficient ? 0.99f : 0.5f;
-        float currScaleValue = 0f;
-        while (currScaleValue < maxScaleValue) {
-            switch (heatedVisualScaleAxis) {
-                case HeatedSegmentScaleAxis.X:
-                    heatedSegmentVisual.localScale += Vector3.right * heatedVisualScaleSpeed * Time.deltaTime;
-                    currScaleValue = heatedSegmentVisual.localScale.x;
-                    break;
-                case HeatedSegmentScaleAxis.Y:
-                    heatedSegmentVisual.localScale += Vector3.up * heatedVisualScaleSpeed * Time.deltaTime;
-                    currScaleValue = heatedSegmentVisual.localScale.y;
-                    break;
-                case HeatedSegmentScaleAxis.Z:
-                    heatedSegmentVisual.localScale += Vector3.forward * heatedVisualScaleSpeed * Time.deltaTime;
-                    currScaleValue = heatedSegmentVisual.localScale.z;
-                    break;
-            }
-
-            yield return null;
-        }
+        yield return new WaitUntil(() => heatedSegment.CurrState == HeatedSegment.State.HEATED);
 
         IsHeated = true;
 
@@ -112,30 +70,6 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
     }
 
     private IEnumerator StopHeating(UnityAction failureCallback) {
-        /*
-        // does not seem to work on oculus quest, possibly due to low framerate; try again after optimization
-        // slow the heating down until it reaches below a certain speed
-        float currSpeed = heatedVisualScaleSpeed;
-        while (currSpeed > 0.1f) {
-            switch (heatedVisualScaleAxis) {
-                case HeatedSegmentScaleAxis.X:
-                    heatedSegmentVisual.localScale += Vector3.right * currSpeed * Time.deltaTime;
-                    break;
-                case HeatedSegmentScaleAxis.Y:
-                    heatedSegmentVisual.localScale += Vector3.up * currSpeed * Time.deltaTime;
-                    break;
-                case HeatedSegmentScaleAxis.Z:
-                    heatedSegmentVisual.localScale += Vector3.forward * currSpeed * Time.deltaTime;
-                    break;
-            }
-
-            // decelerate
-            currSpeed *= 0.99f;
-            print($"{currSpeed}");
-            yield return null;
-        }
-        */
-
         // heating has stopped; wait a moment before starting cooling
         yield return new WaitForSeconds(2f);
 
@@ -152,37 +86,10 @@ public class ShipEngineConnectorSegment : MonoBehaviour {
     }
 
     private IEnumerator StartCooling() {
-        float currSpeed = 0.01f;
-        float currScaleValue = heatedVisualScaleAxis == HeatedSegmentScaleAxis.X
-            ? heatedSegmentVisual.localScale.x
-            : heatedVisualScaleAxis == HeatedSegmentScaleAxis.Y
-                ? heatedSegmentVisual.localScale.y
-                : heatedSegmentVisual.localScale.z;
+        StartCoroutine(heatedSegment.Cool());
 
-        while (currScaleValue > 0.01f) {
-            switch (heatedVisualScaleAxis) {
-                case HeatedSegmentScaleAxis.X:
-                    heatedSegmentVisual.localScale -= Vector3.right * heatedVisualScaleSpeed * Time.deltaTime;
-                    currScaleValue = heatedSegmentVisual.localScale.x;
-                    break;
-                case HeatedSegmentScaleAxis.Y:
-                    heatedSegmentVisual.localScale -= Vector3.up * heatedVisualScaleSpeed * Time.deltaTime;
-                    currScaleValue = heatedSegmentVisual.localScale.y;
-                    break;
-                case HeatedSegmentScaleAxis.Z:
-                    heatedSegmentVisual.localScale -= Vector3.forward * heatedVisualScaleSpeed * Time.deltaTime;
-                    currScaleValue = heatedSegmentVisual.localScale.z;
-                    break;
-            }
+        yield return new WaitUntil(() => heatedSegment.CurrState == HeatedSegment.State.COOLED);
 
-            if (currSpeed < heatedVisualScaleSpeed) {
-                currSpeed *= 1.01f;
-            }
-
-            yield return null;
-        }
-
-        heatedSegmentVisual.gameObject.SetActive(false);
         IsHeated = false;
 
         foreach (ShipEngineConnectorSegment segment in connectedSegments) {
