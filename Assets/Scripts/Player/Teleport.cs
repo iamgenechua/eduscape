1ec82f76d179e571765c5eb776fad0922d16d5d3
Kenneth Fung <fungchenyu@rocketmail.com>
2021-04-13 23:14:47 +0800

    Fix teleport height

--- Assets/Scripts/Player/Teleport.cs
+++ Assets/Scripts/Player/Teleport.cs
@@ -6,8 +6,14 @@ public class Teleport : MonoBehaviour {
 
     private BoxCollider bodyCollider;
 
+    private Transform toMove;
+
     private void Awake() {
         bodyCollider = GetComponent<BoxCollider>();
+    }
+
+    private void Start() {
+        toMove = transform.parent;
     }
 
     /// <summary>
@@ -19,7 +25,9 @@ public class Teleport : MonoBehaviour {
         Vector3 closestPoint = bodyCollider.ClosestPointOnBounds(targetPos);
         // get the distance from that closest point to the target position
         float distanceToTravel = Vector3.Distance(closestPoint, targetPos);
-        // move to the adjusted target position
-        transform.parent.position += (targetPos - transform.position).normalized * distanceToTravel;
+        // calculate the adjusted target position
+        Vector3 adjustedTargetPos = (targetPos - transform.position).normalized * distanceToTravel;
+        // move to the adjusted target position while maintaining the height of the player
+        toMove.position = new Vector3(toMove.position.x + adjustedTargetPos.x, targetPos.y, toMove.position.z + adjustedTargetPos.z);
     }
 }
